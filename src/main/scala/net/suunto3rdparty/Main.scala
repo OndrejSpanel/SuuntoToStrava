33a74822761578b0e985caa3a4cbe1f35711f856
OndrejSpanel <OndrejSpanel@users.noreply.github.com>
2016-03-10 16:56:51 +0100

    Strava token working.

--- src/main/scala/net/suunto3rdparty/Main.scala
+++ src/main/scala/net/suunto3rdparty/Main.scala
@@ -3,8 +3,13 @@ package net.suunto3rdparty
 import moveslink.MovesLinkUploader
 import moveslink2.MovesLink2Uploader
 import org.apache.log4j.Logger
-
 import java.io.File
+import javastrava.api.v3.auth.impl.retrofit.AuthorisationServiceImpl
+import javastrava.api.v3.rest.API
+import javastrava.api.v3.service.Strava
+import javastrava.api.v3.auth.model.Token
+
+import scalaj.http.Http
 
 object Main extends App {
   val log = Logger.getLogger(classOf[App])
@@ -36,10 +41,31 @@ object Main extends App {
 
 object StravaAccess extends App {
   val home = new File(Util.getSuuntoHome, "Moveslink")
+  val appId = 8138
   val tokenFile = new File(home, "strava.id")
 
   val source = scala.io.Source.fromFile(tokenFile)
-  val token = try source.getLines().mkString finally source.close()
+  val (clientSecret, accessToken, code) = try {
+    val lines = source.getLines()
+    val secret = lines.next
+    val token = lines.next
+    val code = lines.next
+    (secret, token, code)
+  } finally source.close()
 
-  println(token)
+  /*
+  val auth = API.authorisationInstance()
+  val response = auth.tokenExchange(appId, clientSecret, code)
+  val token = new Token(response)
+
+  val api = new API(token)
+
+  val athlete = api.getAthlete(11115118)
+  */
+
+  val request = Http("https://www.strava.com/api/v3/oauth/token").postData(s"client_id=$appId&client_secret=$clientSecret&code=$code")
+
+  val tokenString = request.asString
+
+  println(tokenString)
 }
