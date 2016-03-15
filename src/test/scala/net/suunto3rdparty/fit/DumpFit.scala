5628e74942ba84bd6508a9862ae5e8500b1a2c80
OndrejSpanel <OndrejSpanel@users.noreply.github.com>
2016-03-15 09:39:32 +0100

    Added extra information dump (laps...).

--- src/test/scala/net/suunto3rdparty/fit/DumpFit.scala
+++ src/test/scala/net/suunto3rdparty/fit/DumpFit.scala
@@ -1,32 +1,43 @@
 package net.suunto3rdparty
 package fit
 
+import java.io.InputStream
+
 import com.garmin.fit
 import com.garmin.fit._
 import java.time.ZonedDateTime
-import scala.collection.JavaConverters._
 
+import scala.collection.JavaConverters._
 import org.scalatest.{FlatSpec, Matchers}
 
 class DumpFit extends FlatSpec with Matchers {
 
   "Decoder" should "dump a fit file" in {
-
-    val decode = new Decode
     val in = getClass.getResourceAsStream("/decodeTest.fit")
+    decodeFile(in)
+  }
 
+  "Decoder" should "dump extra information from a fit file" in {
+    val in = getClass.getResourceAsStream("/decodeTestExt.fit")
+    decodeFile(in, "record")
+  }
+
+  def decodeFile(in: InputStream, ignoreMessages: String*): Unit = {
+    val decode = new Decode
     try {
       val listener = new MesgListener {
         override def onMesg(mesg: Mesg): Unit = {
-          println(s"${mesg.getName}")
-          val fields = mesg.getFields.asScala
-          for (f <- fields) {
-            f.getName match {
-              case "timestamp" =>
-                val time = new DateTime(f.getLongValue)
-                println(s"  ${f.getName}:${time.toString}")
-              case _ =>
-                println(s"  ${f.getName}:${f.getValue}")
+          if (!ignoreMessages.contains(mesg.getName)) {
+            println(s"${mesg.getName}")
+            val fields = mesg.getFields.asScala
+            for (f <- fields) {
+              f.getName match {
+                case "timestamp" =>
+                  val time = new DateTime(f.getLongValue)
+                  println(s"  ${f.getName}:${time.toString}")
+                case _ =>
+                  println(s"  ${f.getName}:${f.getValue}")
+              }
             }
           }
         }
@@ -35,7 +46,5 @@ class DumpFit extends FlatSpec with Matchers {
     } finally {
       in.close()
     }
-    //mesgBroadcaster.run(in)
   }
-
 }
