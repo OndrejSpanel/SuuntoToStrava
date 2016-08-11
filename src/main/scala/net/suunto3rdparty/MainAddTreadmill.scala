bdf2c61d9a1948d1ea410d3c85a6ea81f939367d
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2016-08-11 21:41:09 +0200

    Fit / Gpx parsing working.

--- src/main/scala/net/suunto3rdparty/MainAddTreadmill.scala
+++ src/main/scala/net/suunto3rdparty/MainAddTreadmill.scala
@@ -1,5 +1,13 @@
 package net.suunto3rdparty
 
+import java.io.{FileInputStream, InputStream, File => IoFile}
+
+import com.garmin.fit._
+
+import scala.xml.XML
+
+//import scala.collection.JavaConverters._
+
 object MainAddTreadmill extends App {
   val routePath = args(0)
   val progressPath = args(1)
@@ -12,9 +20,45 @@ object MainAddTreadmill extends App {
     else progressPath.take(ext) + suffix + progressPath.drop(ext)
   }
   val resultPath = createResultPath(progressPath, "_track")
+
   // parse GPX
+
+  val gpx = XML.loadFile(routePath)
 
 
   // load FIT, output as FIT again, only enriched with the GPX data
-  println(resultPath)
+
+
+  def processFitFile(in: InputStream, out: IoFile): Unit = {
+    val decode = new Decode
+    val encode = new FileEncoder(out)
+    try {
+      val listener = new MesgListener {
+        override def onMesg(mesg: Mesg): Unit = {
+          println(mesg.getName)
+          encode.write(mesg)
+        }
+      }
+      decode.read(in, listener)
+    } finally {
+      in.close()
+      encode.close()
+    }
+  }
+
+  val routeCoordXml = gpx \ "trk" \ "trkseg" \ "trkpt"
+
+  val routeCoord = routeCoordXml.map { rc =>
+    val lat = (rc \ "@lat").text.toDouble
+    val lon = (rc \ "@lon").text.toDouble
+    (lon, lat)
+  }
+
+  val is = new FileInputStream(progressPath)
+  try {
+    processFitFile(is, new IoFile(resultPath))
+  } finally {
+    is.close()
+  }
+
 }
